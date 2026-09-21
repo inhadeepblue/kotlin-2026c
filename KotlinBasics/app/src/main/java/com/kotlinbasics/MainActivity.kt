bfb765281e74939265c602a93de92bc85d51d4bc
deepblue <deepblue@inha.ac.kr>
2026-09-21 15:49:52 +0900

    kotlin basics v0.7 (classes & collections)

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -44,6 +44,16 @@ private fun week04Collections() {
     mutableFruits.add("banana")
     println("Fruits: $fruits")
     println("Mutable Fruits: $mutableFruits")
+
+    val scores = mapOf("Kim" to 100, "Park" to 96, "Lee" to 97)
+    println("Scores: $scores")
+
+    for(fruit in mutableFruits){
+        println("I like $fruit")
+    }
+
+    scores.forEach{(name, score) -> println("$name scored $score")}
+    fruits.forEach { fruit -> println("$fruit") }
 }
 
 private fun week04Classes() {
